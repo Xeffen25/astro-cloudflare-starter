8d5c31bee44b0d482052539ba09d5ba84677c2d6
Xeffen25 <xeffen25@xeffen25.com>
2024-08-05 10:19:30 +0200

    Astro Configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,26 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://astro-cloudflare-starter.pages.dev",
+	scopedStyleStrategy: "class",
+	security: {
+		checkOrigin: true,
+	},
+	build: {
+		assets: "assets",
+		inlineStylesheets: "never",
+	},
+	prefetch: {
+		prefetchAll: false,
+		defaultStrategy: "viewport",
+	},
+	i18n: {
+		defaultLocale: "es",
+		locales: ["es"],
+	},
 	output: "server",
 	adapter: cloudflare({
+		imageService: "compile",
 		platformProxy: {
 			enabled: true,
 		},

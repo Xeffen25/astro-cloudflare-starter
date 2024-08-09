418ec47994ee2642d7011dd43e87dff11d68004e
Xeffen25 <xeffen25@xeffen25.com>
2024-08-09 06:06:49 +0200

    Started Types

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,3 +4,51 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
 	interface Locals extends Runtime {}
 }
+
+interface OpenGraphMusicSong {}
+
+interface OpenGraphMusicAlbum {}
+
+interface OpenGraphMusicPlaylist {}
+
+interface OpenGraphMusicRadioStation {}
+
+interface OpenGraphImage {
+	source: ImageMetadata | URL | string;
+	width: number;
+	height: number;
+	type: string;
+	alt: string;
+}
+
+interface OpenGraphVideo {
+	source: URL | string;
+	width: number;
+	height: number;
+	type: string;
+	alt: string;
+}
+
+interface OpenGraphAudio {
+	source: URL | string;
+	type: string;
+}
+
+interface OpenGraph {
+	title: string;
+	type: string;
+	image: ImageMetadata;
+	url: URL | string;
+	audio: URL | string;
+	description: string;
+	determiner: string;
+	locale: string;
+	locale_alternate: Array<string>;
+	site_name: string;
+	video: string;
+}
+
+interface PageProps {
+	title: string;
+	description: string;
+}
